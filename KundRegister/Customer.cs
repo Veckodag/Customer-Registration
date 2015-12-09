ed53f47945f8509a58bab2914b2d48e5764027e2
Veckodag <Veckodag@gmail.com>
2015-12-09 12:36:30 +0100

    Started POCO

---
+++ KundRegister/Customer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KundRegister
+{
+    public class Customer
+    {
+        public bool IsCompany { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string StreetName { get; set; }
+        public string ZipCode { get; set; }
+        public string City { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public bool WantNewsLetter { get; set; }
+        public string Notes { get; set; }
+        public Guid CustomerNumber { get; set; }
+
+    }
+}
